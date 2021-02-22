a9aa9189eb0172294abb1633c2f8547c1fcf0863
souilhi <siteprogali@gmail.com>
2021-02-22 23:09:28 +0100

    product cat app

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,82 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../../services/products.service';
+import {ProductModel} from '../../model/product.model';
+import {Observable, of} from 'rxjs';
+import {catchError, map, startWith} from 'rxjs/operators';
+import {AppDataState, DataStateEnum} from '../../state/product.state';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  // products: ProductModel[] | null=null;
+  products$: Observable<AppDataState<ProductModel[]>>|null = null;
+  readonly DataStateEnum = DataStateEnum;
+  constructor(private productsService: ProductService) { }
+
+  ngOnInit(): void {
+  }
+
+  onGetAllProducts() {
+    this.products$ = this.productsService.getAllProducts().pipe(
+        map(data => ({dataState: DataStateEnum.LOADED, data})),
+          startWith({dataState: DataStateEnum.LOADING}),
+          catchError(error => of({dataState: DataStateEnum.ERROR, errorMessage: error.message}))
+      );
+      /*this.productsService.getAllProducts().subscribe(data=>{
+        this.products = data;
+        console.log(this.products)
+      },error => {
+        console.log(error)
+      })*/
+  }
+
+  onGetSelectedProducts() {
+    this.products$ = this.productsService.getSelectedProducts().pipe(
+      map(data => ({dataState: DataStateEnum.LOADED, data})),
+      startWith({dataState: DataStateEnum.LOADING}),
+      catchError(error => of({dataState: DataStateEnum.ERROR, errorMessage: error.message}))
+    );
+  }
+
+  onGetAvailableProducts() {
+    this.products$ = this.productsService.getAvailableProducts().pipe(
+      map(data => ({dataState: DataStateEnum.LOADED, data})),
+      startWith({dataState: DataStateEnum.LOADING}),
+      catchError(error => of({dataState: DataStateEnum.ERROR, errorMessage: error.message}))
+    );
+  }
+
+  onSearch(dataForm: any) {
+    this.products$ = this.productsService.searchProducts(dataForm.keyword).pipe(
+      map(data => ({dataState: DataStateEnum.LOADED, data})),
+      startWith({dataState: DataStateEnum.LOADING}),
+      catchError(error => of({dataState: DataStateEnum.ERROR, errorMessage: error.message}))
+    );
+  }
+
+  onSelect(p: ProductModel) {
+    console.log('p 1 ', p);
+    this.productsService.select(p).subscribe(data => {
+      p.selected = data.selected;
+    });
+
+    console.log('p 3', p);
+  }
+
+  onDelete(p: ProductModel) {
+    const v = confirm('etez vous sur suprimer ce produit !');
+    if (v == true) {
+    this.productsService.deleteProduct(p).subscribe(data => {
+      this.onGetAllProducts();
+    });
+    }
+  }
+
+  edit(p: ProductModel) {
+
+  }
+}
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {ProductModel} from '../model/product.model';
+
+
+@Injectable({providedIn: 'root'})
+export class ProductService {
+  constructor(private http: HttpClient) {
+  }
+  getAllProducts(): Observable<ProductModel[]>{
+    const host = environment.host;
+    return this.http.get<ProductModel[]>(host + 'products');
+  }
+  getSelectedProducts(): Observable<ProductModel[]>{
+    const host = environment.host;
+    return this.http.get<ProductModel[]>(host + 'products?selected=true');
+  }
+  getAvailableProducts(): Observable<ProductModel[]>{
+    const host = environment.host;
+    return this.http.get<ProductModel[]>(host + 'products?available=true');
+  }
+  searchProducts(keyword: string): Observable<ProductModel[]>{
+    const host = environment.host;
+    return this.http.get<ProductModel[]>(host + 'products?name_like=' + keyword);
+  }
+  select(selectedProduct: ProductModel): Observable<ProductModel>{
+    const host = environment.host;
+    selectedProduct.selected = !selectedProduct.selected;
+    return this.http.put<ProductModel>(host + 'products/' + selectedProduct.id, selectedProduct);
+  }
+  deleteProduct(selectedProduct: ProductModel): Observable<void>{
+    const host = environment.host;
+    selectedProduct.selected = !selectedProduct.selected;
+    return this.http.delete<void>(host + 'products/' + selectedProduct.id);
+  }
+}
